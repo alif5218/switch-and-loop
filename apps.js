017211e9ad84725a03e347f015d7911e75afbc96
alif5218 <128446391+alif5218@users.noreply.github.com>
2023-05-30 22:40:13 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,109 @@
+
+var alphabet=prompt("Enter Your Letter:");
+switch(alphabet){
+	case 'A':
+	case 'a':
+	case 'E':
+	case 'e':
+	case 'I':
+	case 'i':
+	case 'O':
+	case 'o':
+	case 'U':
+	case 'u':
+	document.write(alphabet +" is Vowel");
+	break;
+	case 'B':
+	case 'b':
+	case 'C':
+    case 'c':
+    case 'D':
+    case 'd':
+    case 'F':
+    case 'f':
+     case 'G':
+     case 'g':
+    case 'H':
+    case 'h':
+    case 'J':
+    case 'j':
+    case 'K':
+    case 'k':
+    case 'L':
+    case 'l':
+    case 'M':
+    case 'm':
+    case 'N':
+    case 'n':
+    case 'n':
+    case 'n':
+        case 'P':
+        case 'p':
+        case 'Q':
+        case 'q':
+        case 'R':
+        case 'r':
+        case 'S':
+        case 's':
+        case 'T':
+        case 't':
+        case 'V':
+        case 'v':
+        case 'W':
+        case 'w':
+        case 'X':
+        case 'x':
+        case 'Y':
+        case 'y':
+        case 'Z':
+        case 'z':
+
+
+
+		document.write(alphabet+' is Consonant');
+	break;
+	default :
+	alert(alphabet +' is not letter!');
+
+	
+}
+
+
+
+
+
+
+
+for(var i=-100;i<=100;i++){
+	console.log(i);
+}
+
+
+
+var i=1;
+while(i<=100){
+	document.write("<br>"+i);
+	i=i+2;
+}
+
+
+
+let total=0;
+for(var i=-100;i<=100;i++){
+	 total+=i;
+}
+document.write("<br>" + total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
